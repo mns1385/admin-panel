bfa6f6d7f0d1d4b926fb7f7da946898e1bc06214
mns1385 <mohammadmns36@gmail.com>
2026-08-17 20:54:47 +0330

    update AppHeader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,11 @@
 <template>
     <header class="h-16 border-b border-gray-200 bg-white px-6">
-        <div class="flex h-full items-center justify-between"></div>
+        <div class="flex h-full items-center justify-between">
+            <div class="flext items-center gap-4">
+                <h1 class="text-xl font-bold text-gray-800">
+                    Admin Panel
+                </h1>
+            </div>
+        </div>
     </header>
 </template>
