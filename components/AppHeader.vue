a7f0c308a9109eca8dc306bc2f4283d70bfff16d
mns1385 <mohammadmns36@gmail.com>
2026-08-17 22:25:57 +0330

    change Header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,10 +1,53 @@
+<script setup>
+    import {Menu, Bell} from 'lucide-vue-next'
+</script>
+
 <template>
     <header class="h-16 border-b border-gray-200 bg-white px-6">
         <div class="flex h-full items-center justify-between">
+
+            <!-- title page -->
             <div class="flext items-center gap-4">
                 <h1 class="text-xl font-bold text-gray-800">
                     Admin Panel
                 </h1>
+            </div>
+
+            <!-- right -->
+            <div class="flex items-center gap-4">
+                <!-- Mobile Menu Button -->
+                <button type="button" class="rounded-lg p-2 text-gray-600 hover:bg-gray-100 sm:hidden" aria-label="Open menu">
+                    <Menu size="20"/>
+                </button>
+
+                <!-- Notification -->
+                <button type="button" class="relative rounded-full p-2 text-gray-500 transition hover:bg-gray-100 hover:text-gray-700" aria-label="Notification">
+                    <Bell size="20"/>
+                    <span class="absolute right-0 top-7 h-2 w-2 rounded-full bg-red-500"></span>
+                </button>
+
+                <!-- User -->
+                <div class="flex items-center gap-3">
+                    <div class="flex h-9 w-9 items-center justify-center rounded-full bg-blue-600 font-semibold text-white">
+                        A
+                    </div>
+                    <div class="hidden sm:block">
+                        <p class="text-sm font-semibold text-gray-800">
+                            Admin
+                        </p>
+                        <p class="text-xs text-gray-500">Administrator</p>
+                    </div>
+                </div>
+
+                <!-- Profile-->
+                <NuxtLink to="/profile" class="hidden rounded-lg px-3 py-2 text-sm font-medium text-gray-600 transition hover:bg-gray-100 hover:text-gray-900 sm:block">
+                    Profile
+                </NuxtLink>
+
+                <!-- Logout -->
+                <button type="button" class="hidden rounded-lg px-3 py-2 text-sm font-medium text-red-600 transition bg-red-100 hover:bg-red-500 hover:text-white sm:block">
+                    Logout
+                </button>
             </div>
         </div>
     </header>
