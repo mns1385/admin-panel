eed16d82743a21dd4394e37f53e692b7d46f5ebe
mns1385 <mohammadmns36@gmail.com>
2026-08-17 20:50:37 +0330

    change /components/AppHeader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <header>
-        App Header
+    <header class="h-16 border-b border-gray-200 bg-white px-6">
+        <div class="flex h-full items-center justify-between"></div>
     </header>
 </template>
