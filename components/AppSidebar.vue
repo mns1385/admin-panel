93281962e1336eea71a6c24eac17022f4f209e8d
mns1385 <mohammadmns36@gmail.com>
2026-08-18 15:17:15 +0330

    update AppSidebar.vue

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -1,22 +1,28 @@
 <template>
     <aside class="h-screen w-64 bg-gray-900 text-white">
         <div class="p-4">
-            <h2 class="text-xl font-bold">Admin Panel</h2>
+            <h2 class="text-xl font-bold">Panel</h2>
         </div>
 
-        <nav class="mt-4">
+        <nav class="mt-4 text-lg">
             <ul>
-                <li>
-                    <NuxtLink to="/">
-                        Dashboard
-                    </NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/profile">
-                        Profile
-                    </NuxtLink>
+                <li v-for="item in menuItems" class="transition px-4 py-2 hover:bg-gray-300 hover:text-black">
+                    <NuxtLink :to="item.path">{{ item.title }}</NuxtLink>
                 </li>
             </ul>
         </nav>
     </aside>
 </template>
+
+<script setup>
+
+const menuItems = [
+    {title: 'Dashboard', path: '/'},
+    {title: 'Users', path: '/users'},
+    {title: 'Products', path: '/products'},
+    {title: 'Orders', path: '/orders'},
+    {title: 'Analytics', path: '/analytics'},
+    {title: 'Settings', path: '/settings'}
+]
+
+</script>
