8f06f91c13f4a90d973d0cb5ca9da0941a1a17af
mns1385 <mohammadmns36@gmail.com>
2026-08-19 00:17:40 +0330

    create sidebar.ts

---
+++ stores/sidebar.ts
@@ -0,0 +1,17 @@
+export const useSidebarStore = defineStore('sidebar', () => {
+    const isOpen = ref(false)
+
+    const toggle = () => {
+        isOpen.value = !isOpen.value
+    }
+
+    const close = () => {
+        isOpen.value = false
+    }
+
+    return {
+        isOpen,
+        toggle,
+        close
+    }
+})
