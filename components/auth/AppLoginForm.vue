edf3efc05c580c98e7c759e8dbab43f386dbf361
mns1385 <mohammadmns36@gmail.com>
2026-08-20 14:06:15 +0330

    update loginform

--- components/auth/AppLoginForm.vue
+++ components/auth/AppLoginForm.vue
@@ -1,19 +1,25 @@
 <template>
     <form class="space-y-5">
         <div>
-            <label for="email" class="mb-2 block text-sm font-medium">
-                Email
+            <label for="email" class="mb-2 block text-xl font-medium text-white text-left">
+                Email :
             </label>
 
-            <input id="email" name="email" type="email" placeholder="Enter your email">
+            <input id="email" name="email" type="email" placeholder="Enter your email"
+            class="block rounded-lg w-full text-xl px-4 py-1 ">
         </div>
 
         <div>
-            <label>
-                Password
+            <label for="password" class="mb-2 block text-xl font-medium text-white text-left">
+                Password :
             </label>
 
-            <input type="password">
+            <input type="password" name="password" id="passwprd" placeholder="Enter your password"
+            class="block rounded-lg w-full text-xl px-4 py-1 ">
         </div>
+
+        <button type="submit" class="w-40 text-white text-xl bg-blue-500 rounded-lg p-2 hover:bg-blue-600 active:bg-blue-700">
+            Login
+        </button>
     </form>
 </template>
--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -7,13 +7,13 @@ definePageMeta({
 </script>
 
 <template>
-    <div class="mx-auto w-full max-w-md rounded-2xl border border-white/10 bg-white/10 p-8 shadow-2xl">
+    <div class="mx-auto w-full max-w-sm rounded-3xl border border-white/10 bg-white/30 p-8 shadow-2xl">
         <div class="mb-8 text-center">
             <h1 class="text-3xl font-bold text-white">
                 Welcome Back
             </h1>
 
-            <p class="mt-2 text-sm text-gray-400">
+            <p class="mt-2 text-sm text-gray-400 mb-5">
                 Sign in to your account
             </p>
 
