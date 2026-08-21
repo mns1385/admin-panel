8360dc01c8e22a4d3990e36cfef65e50848249e6
mns1385 <mohammadmns36@gmail.com>
2026-08-21 04:44:54 +0330

    add middleware to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
+<script setup lang="ts">
+    definePageMeta({
+        middleware: 'auth'
+    })
+</script>
+
 <template>
-    <div class="p-6">
-        <h1 class="text-2xl font-bold">
-            Dashboard
-        </h1>
-    </div>
+    <p>
+        dashboard
+    </p>
 </template>
