0fd134df7e09f78a7f03955095cbbc8ebb642c90
mns1385 <mohammadmns36@gmail.com>
2026-08-22 15:12:11 +0330

    update code header and sidebar

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -19,7 +19,7 @@
 </script>
 
 <template>
-    <header class="h-16 border-b border-gray-200 bg-white px-6">
+    <header class="h-16 border-b border-gray-300 bg-white px-6">
         <div class="flex h-full items-center justify-between">
 
             <!-- title page -->
@@ -27,7 +27,8 @@
                 <h1 class="text-xl font-bold text-gray-800">
                     Admin Panel
                 </h1>
-                <button class="rounded-lg p-2 hover:bg-gray-100 md:hidden" @click="sidebarStore.toggle()">
+                <button class="rounded-lg p-2 hover:bg-gray-100 md:hidden" 
+                @click="sidebarStore.toggle()">
                     <Menu size="20"/>
                 </button>
             </div>
@@ -36,23 +37,30 @@
             <div class="flex items-center gap-4">
                 <!-- Mobile Menu Button -->
                 <div class="relative sm:hidden">
-                    <button type="button" @click="toggleMenu" class="rounded-lg p-2 text-gray-600 hover:bg-gray-100" aria-label="Open menu">
+                    <button type="button" @click="toggleMenu" 
+                    class="rounded-lg p-2 text-gray-600 hover:bg-gray-100" 
+                    aria-label="Open menu">
                         <CircleUserRound size="25"/>
                     </button>
 
-                    <div v-if="isMenuOpen" class="absolute right-0 top-full mt-2 min-w-36 rounded-lg border border-gray-200 bg-white p-2 shadow-lg">
-                        <button @click="goToProfile" class="block w-full rounded-md px-4 py-2 text-left hover:bg-gray-100">
+                    <div v-if="isMenuOpen" 
+                    class="absolute right-0 top-full mt-2 min-w-36 rounded-lg border border-gray-200 bg-white p-2 shadow-lg">
+                        <button @click="goToProfile" 
+                        class="block w-full rounded-md px-4 py-2 text-left hover:bg-gray-100">
                             Profile
                         </button>
 
-                        <button type="button" @click="authStore.logout()" class="block w-full rounded-md px-4 py-2 text-left hover:bg-gray-100">
+                        <button type="button" @click="authStore.logout()" 
+                        class="block w-full rounded-md px-4 py-2 text-left hover:bg-gray-100">
                             Logout
                         </button>
                     </div>
                 </div>
 
                 <!-- Notification -->
-                <button type="button" class="relative rounded-full p-2 text-gray-500 transition hover:bg-gray-100 hover:text-gray-700" aria-label="Notification">
+                <button type="button" 
+                class="relative rounded-full p-2 text-gray-500 transition hover:bg-gray-100 hover:text-gray-700" 
+                aria-label="Notification">
                     <Bell size="20"/>
                     <span class="absolute right-1 top-6 h-2 w-2 rounded-full bg-red-500"></span>
                 </button>
@@ -71,12 +79,14 @@
                 </div>
 
                 <!-- Profile-->
-                <NuxtLink to="/profile" class="hidden rounded-lg px-3 py-2 text-sm font-medium text-gray-600 transition hover:bg-gray-100 hover:text-gray-900 sm:block">
+                <NuxtLink to="/profile" 
+                class="hidden rounded-lg px-3 py-2 text-sm font-medium text-gray-600 transition hover:bg-gray-100 hover:text-gray-900 sm:block">
                     Profile
                 </NuxtLink>
 
                 <!-- Logout -->
-                <button type="button" @click="authStore.logout()" class="hidden rounded-lg px-3 py-2 text-sm font-medium text-red-600 transition bg-red-100 hover:bg-red-500 hover:text-white sm:block">
+                <button type="button" @click="authStore.logout()" 
+                class="hidden rounded-lg px-3 py-2 text-sm font-medium text-red-600 transition bg-red-100 hover:bg-red-500 hover:text-white sm:block">
                     Logout
                 </button>
             </div>
--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -15,7 +15,10 @@ const menuItems = [
 </script>
 
 <template>
-    <aside :class="['fixed left-0 top-16 z-40 h-screen w-64 bg-gray-200 text-black', 'transition-transform duration-300', sidebarStore.isOpen ? 'translate-x-0' : '-translate-x-full', 'md:static md:translate-x-0']">
+    <aside :class="['fixed left-0 top-16 z-40 h-screen w-64 bg-gray-200 text-black',
+     'transition-transform duration-300',
+      sidebarStore.isOpen ? 'translate-x-0' : '-translate-x-full',
+       'md:static md:translate-x-0']">
         <div class="p-4">
             <h1 class="text-2xl font-bold">Workspace</h1>
         </div>
@@ -23,7 +26,11 @@ const menuItems = [
         <nav class="mt-4 text-lg">
             <ul>
                 <li v-for="item in menuItems">
-                    <NuxtLink @click="sidebarStore.close()" :to="item.path" class="transition block px-4 py-2 hover:bg-gray-300 hover:text-black" active-class="bg-gray-300 text-black">{{ item.title }}</NuxtLink>
+                    <NuxtLink @click="sidebarStore.close()" :to="item.path" 
+                    class="transition block px-4 py-2 hover:bg-gray-600 hover:text-white" 
+                    active-class="bg-gray-600 text-white">
+                        {{ item.title }}
+                    </NuxtLink>
                 </li>
             </ul>
         </nav>
