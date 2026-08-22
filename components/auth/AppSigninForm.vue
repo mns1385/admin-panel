462c2ab1269e9493d0c63c9cec2e2325e207ff20
mns1385 <mohammadmns36@gmail.com>
2026-08-23 00:35:17 +0330

    create AppSigninForm

---
+++ components/auth/AppSigninForm.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+    
+</script>
+
+<template>
+    <form>
+        <div>
+            <label for="email">
+                Email :
+            </label>
+            <input type="email" name="email" id="email">
+        </div>
+
+        <div>
+            <label for="username">
+                UserName :
+            </label>
+            <input type="text" name="username" id="username">
+        </div>
+
+        <div>
+            <label for="password">
+                Password :
+            </label>
+            <input type="password" name="password" id="password">
+        </div>
+
+        <button type="submit">
+            Signin
+        </button>
+    </form>
+</template>
