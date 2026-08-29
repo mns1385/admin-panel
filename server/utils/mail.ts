932a5ac44433ef67b5b3c94304e9501eb09e36e9
mns1385 <mohammadmns36@gmail.com>
2026-08-30 01:34:52 +0330

    create mail.ts

---
+++ server/utils/mail.ts
@@ -0,0 +1,23 @@
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+export async function sendVerificationCode( email: string, code: number ) {
+    const { data, error} = await resend.emails.send({
+        from: 'onboarding@resend.dev',
+        to: email,
+        subject: 'Verify code',
+        html: `
+        <div>
+            <h2>Your verify code for admin-panel</h2>
+
+            <h1>${code}</h1>
+        </div>`
+    })
+
+    if (error) {
+        throw new Error(error.message)
+    }
+
+    return data
+}
