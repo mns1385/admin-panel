6ecf54e6b4ad47471a22bec759804e1874db6513
mns1385 <mohammadmns36@gmail.com>
2026-09-04 18:50:17 +0330

    install vue3-toastify

---
+++ plugins/vue3-toastify.ts
@@ -0,0 +1,17 @@
+import { defineNuxtPlugin } from '#app'
+import Vue3Toastify, { toast } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
+
+export default defineNuxtPlugin((nuxtApp) => {
+    nuxtApp.vueApp.use(Vue3Toastify, {
+        autoClose: 3000,
+        position: 'top-right',
+        theme: 'auto'
+    })
+
+    return {
+        provide: {
+            toast
+        }
+    }
+})
