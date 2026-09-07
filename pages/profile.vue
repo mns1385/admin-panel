97abda92b671ca0c07828d1e422d06dce64219da
mns1385 <mohammadmns36@gmail.com>
2026-09-08 00:37:24 +0330

    update header tabs

--- pages/profile.vue
+++ pages/profile.vue
@@ -60,7 +60,7 @@ const isOpenMenu = ref(false)
 
                     <!--Tabs header-->
                     <div class="flex items-center justify-between mb-4">
-                        <h2 class="text-lg font-semibold text-gray-700">
+                        <h2 class="text-2xl font-semibold text-gray-600">
                             Tabs
                         </h2>
 
