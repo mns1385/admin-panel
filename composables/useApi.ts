5760768242995572c67fb35fa7ad73f44d11c42f
mns1385 <mohammadmns36@gmail.com>
2026-09-22 12:24:44 +0330

    add order methods to useApi

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -110,11 +110,127 @@ export const useApi = () => {
         }
     }
 
+
+    const getOrders = async () => {
+        try {
+            const data = await $fetch(`${baseUrl}/orders`)
+
+            return data
+        } catch (error) {
+            throw createError({
+                statusCode: 404,
+                statusMessage: 'Error in get orders!'
+            })
+        }
+    }
+
+    const getOrder = async (id: string) => {
+
+        if (!id) {
+            throw createError({
+                statusCode: 400,
+                statusMessage: 'Id for order is required!'
+            })
+        }
+
+        try {
+            const data = await $fetch(`${baseUrl}/orders/${id}`)
+
+            return data
+        } catch (error) {
+            throw createError({
+                statusCode: 404,
+                statusMessage: 'Error in get orders!'
+            })
+        }
+    }
+
+    const createOrder = async (order: {userId: string, name: string, status: string, amount: number, date: string, number: number}) => {
+
+        if (!order) {
+            throw createError({
+                statusCode: 400,
+                statusMessage: 'Order is required'
+            })
+        }
+
+        try {
+            const data = await $fetch(`${baseUrl}/orders`, {
+                method: 'POST',
+                body: order
+            })
+
+            return data
+        } catch (error) {
+            throw createError({
+                statusCode: 404,
+                statusMessage: 'Error in create order!'
+            })
+        }
+    }
+
+    const updateOrder = async (id: string, order: {userId: string, name: string, status: string, amount: number, date: string, number: number}) => {
+        if (!id) {
+            throw createError({
+                statusCode: 400,
+                statusMessage: 'Id for user is required!'
+            })
+        }
+
+        if (!order) {
+            throw createError({
+                statusCode: 400,
+                statusMessage: 'Order is required'
+            })
+        }
+
+        try {
+            await $fetch(`${baseUrl}/orders/${id}`, {
+                method: 'PUT',
+                body: order
+            })
+
+        } catch (error) {
+            throw createError({
+                statusCode: 404,
+                statusMessage: 'Error in update order server!'
+            })
+        }
+    }
+
+    const deleteOrder = async (id: string) => {
+        if (!id) {
+            throw createError({
+                statusCode: 400,
+                statusMessage: 'Id for order is required!'
+            })
+        }
+
+        try {
+            const data = await $fetch(`${baseUrl}/orders/${id}`, {
+                method: 'DELETE'
+            })
+
+            return data
+        } catch (error) {
+            throw createError({
+                statusCode: 404,
+                statusMessage: 'Error in delete order!'
+            })
+        }
+    }
+
     return {
         getUsers,
         getUser,
         createUser,
         updateUser,
-        deleteUser
+        deleteUser,
+        
+        getOrders,
+        getOrder,
+        createOrder,
+        updateOrder,
+        deleteOrder
     }
 }
