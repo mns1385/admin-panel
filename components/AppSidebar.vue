99d4fb4fc7a7b0c9202c9a1c9e16213f0e98c4a4
mns1385 <mohammadmns36@gmail.com>
2026-09-23 01:05:15 +0330

    update sidebar

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -1,17 +1,27 @@
 <script setup lang="ts">
 import { useSidebarStore } from '~/stores/sidebar';
+import { useProfileStore } from '~/stores/profile';
 
 const sidebarStore = useSidebarStore()
+const profileStore = useProfileStore()
 
-const menuItems = [
+const user = computed(() => profileStore.user || {role: 'user'})
+
+onMounted(async () => {
+    await profileStore.onLoad()
+})
+
+const menuItems = user.value.role === 'admin'? [
     {title: 'Dashboard', path: '/'},
     {title: 'Users', path: '/users'},
     {title: 'Products', path: '/products'},
     {title: 'Orders', path: '/orders'},
     {title: 'Analytics', path: '/analytics'},
     {title: 'Settings', path: '/settings'}
+]: [
+    {title: 'Dashboard', path: '/'},
+    {title: 'Products', path: '/products'}
 ]
-
 </script>
 
 <template>
